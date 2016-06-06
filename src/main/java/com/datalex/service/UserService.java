ab0d40f0d1d84fea60877ff6c1227f4af3c3cacd
mirror <mironosk@gmail.com>
2016-06-06 11:17:38 +0300

    Initial commit

---
+++ src/main/java/com/datalex/service/UserService.java
@@ -0,0 +1,24 @@
+package com.datalex.service;
+
+
+import com.datalex.bean.User;
+import com.datalex.dao.UserDao;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/UserService")
+public class UserService {
+
+    UserDao userDao = new UserDao();
+
+    @GET
+    @Path("/users")
+    @Produces(MediaType.APPLICATION_XML)
+    public List<User> getUsers(){
+        return userDao.getAllUsers();
+    }
+}
